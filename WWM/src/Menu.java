96edbc0f6e2078b531e2ba3fe094b155f53953da
ri1643 <recep.inanir@ozu.edu.tr>
2015-12-07 17:50:43 +0200

    Update Menu.java

--- WWM/src/Menu.java
+++ WWM/src/Menu.java
@@ -1,4 +1,33 @@
+public class Main extends JFrame {
 
-public class Menu {
+    public Main() throws IOException {
 
+    	setTitle("Kim Milyoner Olmak İster?");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600)
+        setResizable(false);
+        setLocationRelativeTo(null);
+        Menu menu = new Menu(800, 600);
+        add(menu);
+        Game game = new Game(menu);
+        menu.game = game;
+    }
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+                Main ps = null;
+				try {
+					ps = new Main();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+                ps.setVisible(true);
+            }
+        });
+    }
 }
