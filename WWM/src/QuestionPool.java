e01fd84731ec743d155b0ed8e5fcd0a9aa2a7668
oguzhanterzioglu <oguzhan.terzioglu@ozu.edu.tr>
2015-12-09 14:27:11 +0200

    Update QuestionPool.java

--- WWM/src/QuestionPool.java
+++ WWM/src/QuestionPool.java
@@ -1,4 +1,12 @@
-
 public class QuestionPool {
 
-}
+     public ArrayList<Question> questions;
+     public HashMap<Integer, ArrayList<Question>> questionPool;
+    
+     public QuestionPool() {
+         
+          questions = new ArrayList<Question>();
+          questionPool = new HashMap<Integer, ArrayList<Question>>();
+          populateQuestions();
+     }
+    
