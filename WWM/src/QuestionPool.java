06e17efc3c294ddbc651c9f25e528b26cc78401c
oguzhanterzioglu <oguzhan.terzioglu@ozu.edu.tr>
2015-12-11 18:44:25 +0200

    Update QuestionPool.java

--- WWM/src/QuestionPool.java
+++ WWM/src/QuestionPool.java
@@ -9,4 +9,23 @@ public class QuestionPool {
           questionPool = new HashMap<Integer, ArrayList<Question>>();
           populateQuestions();
      }
+     public void addQuestion(Question q) {
+         
+          questions.add(q);
+     }
+    
+     public Question getQuestion(int index) {
+          if (questionPool.get(index) != null) {
+               Random random = new Random();
+               ArrayList<Question> list = questionPool.get(index);
+               return list.get(random.nextInt(list.size()));
+          }
+         
+          return null;
+     }
+    
+     public void populateQuestions(){
+          for (int i = 0; i < 12; i++) {
+               questionPool.put(i, new ArrayList<Question>());
+          }
     
